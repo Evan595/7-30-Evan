87d030e6b844abd275237a2bdbafbe4d5ff57b6e
Evan595 <53194327+Evan595@users.noreply.github.com>
2019-07-30 21:25:16 -0500

    7-30-Evan

---
+++ src/Cat.kt
@@ -0,0 +1,15 @@
+class Cat(var name: String? = ""){
+    fun Meow() { println("Meow!")}
+}
+
+fun main(args: Array<String>){
+    var myCats = arrayOf(Cat("Misty"),
+                        null,
+                        Cat("Socks"))
+        for (cat in myCats){
+            if (cat != null){
+                print("${cat.name}:")
+                cat.Meow()
+            }
+        }
+}
